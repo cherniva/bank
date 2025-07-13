de44f871709ee14f441acb1bcf362e8b95be69a2
Ivan Chernikov <chernikoviivan@gmail.com>
2025-07-13 12:35:31 +0200

    Implement client credentials flow oauth2 authorization via gateway

---
+++ api-gateway/src/main/java/com/cherniva/apigateway/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.cherniva.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll());
+        return http.build();
+    }
+}
--- api-gateway/src/main/java/com/cherniva/apigateway/controller/PrinterController.java
+++ api-gateway/src/main/java/com/cherniva/apigateway/controller/PrinterController.java
@@ -1,24 +1,24 @@
-package com.cherniva.apigateway.controller;
-
-import com.cherniva.apigateway.config.GatewayConfigProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/texts")
-@RefreshScope
-public class PrinterController {
-
-    private final GatewayConfigProperties config;
-
-    public PrinterController(GatewayConfigProperties config) {
-        this.config = config;
-    }
-
-    @GetMapping
-    public String getText() {
-        return config.getRoutes().get(0).getId(); // or uri, etc.
-    }
-}
+//package com.cherniva.apigateway.controller;
+//
+//import com.cherniva.apigateway.config.GatewayConfigProperties;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/texts")
+//@RefreshScope
+//public class PrinterController {
+//
+//    private final GatewayConfigProperties config;
+//
+//    public PrinterController(GatewayConfigProperties config) {
+//        this.config = config;
+//    }
+//
+//    @GetMapping
+//    public String getText() {
+//        return config.getRoutes().get(0).getId(); // or uri, etc.
+//    }
+//}
