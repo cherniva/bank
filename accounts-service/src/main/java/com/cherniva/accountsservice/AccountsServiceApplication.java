a5399077cf37abbcd592bee6318fdc4fb247063d
Ivan Chernikov <chernikoviivan@gmail.com>
2025-07-14 19:14:19 +0200

    Fix configuration

--- accounts-service/src/main/java/com/cherniva/accountsservice/AccountsServiceApplication.java
+++ accounts-service/src/main/java/com/cherniva/accountsservice/AccountsServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.cherniva.common.model")
+@SpringBootApplication
+@ComponentScan({"com.cherniva.common", "com.cherniva.accountsservice"})
 @EnableDiscoveryClient
 @EntityScan(basePackages = {"com.cherniva.common.model"})
-@EnableJpaRepositories(basePackages = {"com.cherniva.common.repository"})
+@EnableJpaRepositories(basePackages = {"com.cherniva.common.repo"})
 public class AccountsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountsServiceApplication.class, args);
