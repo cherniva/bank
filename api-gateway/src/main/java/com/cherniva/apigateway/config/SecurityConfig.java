29c76c41a149bbb81b0b71cd6b1d16766aad9fcb
lebmax <lebmax@gmail.com>
2025-07-16 23:52:47 +0300

    Переписал авторизацию

--- api-gateway/src/main/java/com/cherniva/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/cherniva/apigateway/config/SecurityConfig.java
@@ -1,24 +1,67 @@
 package com.cherniva.apigateway.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@EnableWebSecurity
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors(withDefaults())
+//                .csrf(withDefaults())
+//                .authorizeHttpRequests(exchanges -> exchanges
+//                        .requestMatchers("/login", "/actuator/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//            .oauth2Login(withDefaults())
+//            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
+//
+//        return http.build();
+//    }
+    
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-            .csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(exchanges -> exchanges
-                .pathMatchers("/login", "/actuator/**").permitAll()
-                .anyExchange().authenticated()
-            )
-            .oauth2Login(withDefaults())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()));
-
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/swagger-ui/**", "/v3/api-docs*/**").permitAll()
+                        .pathMatchers("/actuator/**", "/fallback/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
         return http.build();
+    }
+    
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        jwtConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        return jwtConverter;
+    }
+    
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user-id"))
+                .defaultIfEmpty("anonymous");
     }
 }
