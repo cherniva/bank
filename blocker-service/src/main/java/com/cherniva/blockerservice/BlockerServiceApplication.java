4437b3c7374a52c7222d2c13d92f318453cae0cd
Ivan Chernikov <chernikoviivan@gmail.com>
2025-07-16 19:39:35 +0200

    Implement blocker service

---
+++ blocker-service/src/main/java/com/cherniva/blockerservice/BlockerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cherniva.blockerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BlockerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlockerServiceApplication.class, args);
+    }
+}
---
+++ blocker-service/src/main/java/com/cherniva/blockerservice/controller/BlockerController.java
@@ -0,0 +1,15 @@
+package com.cherniva.blockerservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping("/blocker/check")
+public class BlockerController {
+    @GetMapping
+    public boolean check() {
+        return ThreadLocalRandom.current().nextBoolean();
+    }
+}
