6c8390af37aa0b7c0b47de8e5c2a67a6eb041145
Ivan Chernikov <chernikoviivan@gmail.com>
2025-08-06 19:09:40 +0200

    Fix containerization for gateway, accounts, front, eureka

--- init.sql
+++ init.sql
@@ -1,3 +1,12 @@
+-- Create schemas
 CREATE SCHEMA IF NOT EXISTS accounts;
 CREATE SCHEMA IF NOT EXISTS cash;
 CREATE SCHEMA IF NOT EXISTS transfer;
+
+-- Grant permissions
+GRANT ALL PRIVILEGES ON SCHEMA accounts TO postgres;
+GRANT ALL PRIVILEGES ON SCHEMA cash TO postgres;
+GRANT ALL PRIVILEGES ON SCHEMA transfer TO postgres;
+
+-- Set search path (optional)
+ALTER DATABASE bank SET search_path TO accounts, cash, transfer, public;
