bcc51b38e89c2acd28bdcd137bc375635e0465fa
Ivan Chernikov <chernikoviivan@gmail.com>
2025-08-06 20:07:54 +0200

    Set up containerization

--- accounts-service/src/main/java/com/cherniva/accountsservice/config/DataInitializer.java
+++ accounts-service/src/main/java/com/cherniva/accountsservice/config/DataInitializer.java
@@ -56,59 +56,59 @@ public class DataInitializer implements CommandLineRunner {
             log.info("Created CNY currency");
 
             // Create user
-            log.info("Creating user...");
-            UserDetails user = new UserDetails();
-            user.setId(SeqGenerator.getNextUserDetails());
-            user.setUsername("1");
-            user.setPassword(passwordEncoder.encode("1"));
-            user.setName("John");
-            user.setSurname("Doe");
-            user.setBirthdate(LocalDate.of(1990, 12, 13));
-
-            UserDetails user1 = new UserDetails();
-            user1.setId(SeqGenerator.getNextUserDetails());
-            user1.setUsername("ch");
-            user1.setPassword(passwordEncoder.encode("ch"));
-            user1.setName("Ivan");
-            user1.setSurname("Chernikov");
-            user1.setBirthdate(LocalDate.of(2000, 6, 13));
-
-            // Create accounts
-            log.info("Creating accounts...");
-            Account account1 = new Account();
-            account1.setId(SeqGenerator.getNextAccount());
-            account1.setCurrency(usd);
-            account1.setUserDetails(user);
-            account1.setAmount(BigDecimal.valueOf(100L));
-            account1.setActive(true);
-
-            Account account2 = new Account();
-            account2.setId(SeqGenerator.getNextAccount());
-            account2.setCurrency(yen);
-            account2.setUserDetails(user);
-            account2.setAmount(BigDecimal.valueOf(100L));
-            account2.setActive(true);
-
-            Account account3 = new Account();
-            account3.setId(SeqGenerator.getNextAccount());
-            account3.setCurrency(rub);
-            account3.setUserDetails(user);
-            account3.setAmount(BigDecimal.valueOf(100L));
-            account3.setActive(true);
-
-            user.setAccounts(Arrays.asList(account1, account2, account3));
-            userDetailsRepo.save(user);
-            syncService.syncUserCreation(user);
-//            for (var account : user.getAccounts()) {
-//                syncService.syncCreation(account);
-//            }
-            log.info("Created user with accounts");
-            userDetailsRepo.save(user1);
-            syncService.syncUserCreation(user1);
-
-            System.out.println("Sample data initialized!");
-            System.out.println("Username: 1, Password: 1");
-            System.out.println("Username: ch, Password: ch");
+//            log.info("Creating user...");
+//            UserDetails user = new UserDetails();
+//            user.setId(SeqGenerator.getNextUserDetails());
+//            user.setUsername("1");
+//            user.setPassword(passwordEncoder.encode("1"));
+//            user.setName("John");
+//            user.setSurname("Doe");
+//            user.setBirthdate(LocalDate.of(1990, 12, 13));
+//
+//            UserDetails user1 = new UserDetails();
+//            user1.setId(SeqGenerator.getNextUserDetails());
+//            user1.setUsername("ch");
+//            user1.setPassword(passwordEncoder.encode("ch"));
+//            user1.setName("Ivan");
+//            user1.setSurname("Chernikov");
+//            user1.setBirthdate(LocalDate.of(2000, 6, 13));
+//
+//            // Create accounts
+//            log.info("Creating accounts...");
+//            Account account1 = new Account();
+//            account1.setId(SeqGenerator.getNextAccount());
+//            account1.setCurrency(usd);
+//            account1.setUserDetails(user);
+//            account1.setAmount(BigDecimal.valueOf(100L));
+//            account1.setActive(true);
+//
+//            Account account2 = new Account();
+//            account2.setId(SeqGenerator.getNextAccount());
+//            account2.setCurrency(yen);
+//            account2.setUserDetails(user);
+//            account2.setAmount(BigDecimal.valueOf(100L));
+//            account2.setActive(true);
+//
+//            Account account3 = new Account();
+//            account3.setId(SeqGenerator.getNextAccount());
+//            account3.setCurrency(rub);
+//            account3.setUserDetails(user);
+//            account3.setAmount(BigDecimal.valueOf(100L));
+//            account3.setActive(true);
+//
+//            user.setAccounts(Arrays.asList(account1, account2, account3));
+//            userDetailsRepo.save(user);
+//            syncService.syncUserCreation(user);
+////            for (var account : user.getAccounts()) {
+////                syncService.syncCreation(account);
+////            }
+//            log.info("Created user with accounts");
+//            userDetailsRepo.save(user1);
+//            syncService.syncUserCreation(user1);
+//
+//            System.out.println("Sample data initialized!");
+//            System.out.println("Username: 1, Password: 1");
+//            System.out.println("Username: ch, Password: ch");
             log.info("Data initialization completed successfully!");
         } catch (Exception e) {
             log.error("Error during data initialization", e);
